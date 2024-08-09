19ba36f51da6ad0c8cda32108d27682c5d2111f4
Corentin CAILLEAUD <corentin.cailleaud@caillef.com>
2024-08-09 16:16:56 +0200

    Update world.lua

--- world.lua
+++ world.lua
@@ -125,6 +125,7 @@ function startGame(toriiClient)
 	-- match everything
 	local clauseJsonStr = "[{ \"Keys\": { \"keys\": [], \"models\": [], \"pattern_matching\": \"VariableLen\" } }]"
 	toriiClient:OnEntityUpdate(clauseJsonStr, function(entities)
+		print("entity update received")
 		for key,newEntity in pairs(entities) do
 			local entity = getOrCreatePlayerEntity(key, newEntity)
 			if entity then entity:update(newEntity) end
